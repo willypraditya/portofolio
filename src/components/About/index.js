007628d26570c0c134f9fd860ae736af790ad294
Willy <w.praditya@yahoo.com>
2021-03-01 02:51:30 +0700

    remove unused assets and codes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,11 +5,6 @@ import { Element } from "react-scroll";
 import profilePicture from "../../assets/profile-picture.jpg";
 import "./about.scss";
 
-import reactLogo from "../../assets/react-logo.svg";
-import webpackLogo from "../../assets/webpack-logo.svg";
-import vscodeLogo from "../../assets/vscode-logo.svg";
-import sassLogo from "../../assets/sass-logo.svg";
-
 const About = () => {
   return (
     <Element name="about">
@@ -26,30 +21,16 @@ const About = () => {
           </Col>
           <Col xs={24} sm={24} md={20} lg={20} xl={20} xxl={16}>
             <p className="about__description">
-              A Front End Developer from Jakarta, currently working as a Junior
-              Front End Engineer at PT. Payfazz Teknologi Nusantara. An active
-              student studying Computer Science at BINUS International
-              University.
+              A recent graduate of BINUS International University's Computer
+              Science program. A former Software Engineer Intern at PT. Modal
+              Rakyat Indonesia. Currently working as a Front-End Engineer at PT.
+              Payfazz Teknologi Nusantara.
             </p>
             <p className="about__description">
               Have an idea or project you want to talk about? Hit me up!
             </p>
           </Col>
         </Row>
-        {/* <Row type="flex" justify="space-around">
-        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
-          <img className="about__tools__vscode" src={vscodeLogo} />
-        </Col>
-        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
-          <img className="about__tools__react" src={reactLogo} />
-        </Col>
-        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
-          <img className="about__tools__webpack" src={webpackLogo} />
-        </Col>
-        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
-          <img className="about__tools__sass" src={sassLogo} />
-        </Col>
-      </Row> */}
       </Card>
     </Element>
   );
